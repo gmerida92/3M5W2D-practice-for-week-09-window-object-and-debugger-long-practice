9dcfa3e029afa09ec8c20bc7f3af8baf2c0874c0
irelius <kihong711@hotmail.com>
2022-05-17 20:19:22 -0700

    phase 3 done

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,13 +1,16 @@
 export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
-    // Your code here
+    document.title = "George and Sam's Portfolio"
 }
 
 export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    let headerDiv = document.body.children[0];
+    let h1 = headerDiv.children[0];
+    h1.innerText = "George and Sam"
 }
 
 export function changeAboutMe() {
@@ -15,4 +18,7 @@ export function changeAboutMe() {
      passage about yourself */
 
     // Your code here
+    let sectionDiv = document.body.children[1];
+    let paragraph1 = sectionDiv.children[1];
+    paragraph1.innerText = "George enjoy the intake of media which includes games, shows, comics, and movies. George also enjoy working on personal development. Samuel just wants to play video games."
 }
