9ec803fd5a3f0786fa28a6affbfcf9fc43239b20
irelius <kihong711@hotmail.com>
2022-05-17 20:54:14 -0700

    bonus phase done?

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,29 +1,39 @@
 export function findElementById(id) {
     // Return the element in the DOM with corresponding `id`
 
-    // Your code here
+    return document.getElementById(id);
 }
 
-export function findFirstElementOfTag(tag) {
-    // Return the first occurence of an element of tag name `tag`
 
-    // Your code here
-}
-
-export function findFirstElementOfClass(cls) {
-    // Return the first occurence of an element of class `cls`
-
-    // Your code here
-}
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
 
-    // Your code here
+    return document.getElementsByTagName(tag)
 }
+
+
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
 
-    // Your code here
+    return document.getElementsByClassName(cls);
+}
+
+
+
+export function findFirstElementOfTag(tag) {
+    // Return the first occurence of an element of tag name `tag`
+
+    let tagArray = document.getElementsByTagName(tag);
+    return tagArray[0];
+}
+
+
+
+export function findFirstElementOfClass(cls) {
+    // Return the first occurence of an element of class `cls`
+
+    let classArray = document.getElementsByClassName(cls);
+    return classArray[0];
 }
